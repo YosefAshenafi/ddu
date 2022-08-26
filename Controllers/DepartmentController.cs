e0fd92612d041579208640526ed67353a9237e92
jos <7629076+YosefAshenafi@users.noreply.github.com>
2022-08-26 16:08:17 +0300

    finished department CRUD

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,53 @@
+﻿using DDU.Data;
+using DDU.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DDU.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        db_a676b9_dduContext db = new db_a676b9_dduContext();
+        public IActionResult Index()
+        {
+            var allDepartments = db.Departments.Where(e => e.IsActive == true).ToList();
+            ViewData["allDepartments"] = allDepartments;
+            return View();
+        }
+        public IActionResult AddDepartment()
+        {
+            ViewData["Departments"] = db.Departments.ToList();
+            return View();
+        }
+        public IActionResult EditDepartment(int departmentId)
+        {
+            ViewData["Departments"] = db.Departments.Where(e => e.DepartmentId != departmentId || e.ParentDepartmentId != departmentId).ToList();
+            var department = db.Departments.FirstOrDefault(d => d.DepartmentId == departmentId);
+            return View(department);
+        }
+        [HttpPost]
+        public IActionResult SaveDepartment(Department dept)
+        {
+            if (dept.DepartmentId == 0)
+            {
+                db.Departments.Update(dept);
+            }
+            else
+            {
+                db.Departments.Update(dept);
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteDepartment(int departmentId)
+        {
+            var department = db.Departments.FirstOrDefault(d => d.DepartmentId == departmentId);
+            if (department != null)
+            {
+                department.IsActive = false;
+                db.Departments.Update(department);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/db_a676b9_dduContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DDU.Models
+{
+    public partial class db_a676b9_dduContext : DbContext
+    {
+        public db_a676b9_dduContext()
+        {
+        }
+
+        public db_a676b9_dduContext(DbContextOptions<db_a676b9_dduContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Department> Departments { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server= sql5097.site4now.net;Database=db_a676b9_ddu;user id=db_a676b9_ddu_admin;password=P@ssw0rd;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.ToTable("Department");
+
+                entity.Property(e => e.DepartmentId).HasColumnName("DepartmentID");
+
+                entity.Property(e => e.DepartmentCode).HasMaxLength(50);
+
+                entity.Property(e => e.DepartmentName).HasMaxLength(50);
+
+                entity.Property(e => e.DisplayDepartmentId).HasColumnName("DisplayDepartmentID");
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.IsActive)
+                    .IsRequired()
+                    .HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.ParentDepartmentId).HasColumnName("ParentDepartmentID");
+
+                entity.Property(e => e.Phone).HasMaxLength(50);
+
+                entity.Property(e => e.RootDepartmentId).HasColumnName("RootDepartmentID");
+
+                entity.Property(e => e.SessionId)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("SessionID");
+
+                entity.Property(e => e.SessionIp)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("SessionIP");
+
+                entity.Property(e => e.SessionMac)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("SessionMAC");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
